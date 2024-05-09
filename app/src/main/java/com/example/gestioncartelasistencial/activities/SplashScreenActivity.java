a283bf30543c652c0a11acec0275e55f044ad2be
AlbertoSB00 <sanchezbaronaalberto560@gmail.com>
2024-05-09 11:01:15 +0200

    mini cambio splash screen

--- app/src/main/java/com/example/gestioncartelasistencial/activities/SplashScreenActivity.java
+++ app/src/main/java/com/example/gestioncartelasistencial/activities/SplashScreenActivity.java
@@ -43,7 +43,7 @@ public class SplashScreenActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    sleep(3000);
+                    sleep(2000);
                     Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivity(intent);
                     finish();
